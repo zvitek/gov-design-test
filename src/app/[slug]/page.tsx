c1334e440d90e41598f2749d40ce861c1abb3572
Pavelhaj93 <pavelhajduch93@gmail.com>
2024-04-30 14:27:56 +0200

    fix: fix useSearchParams

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -4,9 +4,7 @@ export default function SlugPage({ params }: { params: { slug: string } }) {
   return (
     <main className="flex h-20 flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold">Slug page</h1>
-      <Suspense>
-        <p>Slug: {params.slug}</p>
-      </Suspense>
+      <p>Slug: {params.slug}</p>
     </main>
   );
 }
--- src/components/Navbar/HeaderSearch/index.tsx
+++ src/components/Navbar/HeaderSearch/index.tsx
@@ -1,18 +1,15 @@
 import React, { useRef } from "react";
-import { useSearchParams } from "next/navigation";
 import Search from "@/components/Search";
 
 const HeaderSearch = () => {
   const ref = useRef<HTMLFormElement>(null);
-  const searchParams = useSearchParams();
-  const search = searchParams.get("q") ?? "";
 
   return (
     <form ref={ref} className="w-full lg:w-[325px]">
       <Search
         placeholder="Hledat"
         name={"q"}
-        value={String(search)}
+        value={"search"}
         size="m"
         onSearch={(search) => {
           ref.current?.submit();
