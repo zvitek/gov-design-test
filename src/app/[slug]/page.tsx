55eeba8a9309025c82916ec479aeafdd24fd09c8
Pavelhaj93 <pavelhajduch93@gmail.com>
2024-04-30 14:25:26 +0200

    feat: fix suspense

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,8 +1,12 @@
+import { Suspense } from "react";
+
 export default function SlugPage({ params }: { params: { slug: string } }) {
   return (
     <main className="flex h-20 flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold">Slug page</h1>
-      <p>Slug: {params.slug}</p>
+      <Suspense>
+        <p>Slug: {params.slug}</p>
+      </Suspense>
     </main>
   );
 }
